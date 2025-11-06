2b9350b079203de62ca66c6f95ec6cf498425a64
jfang <im.jonathan.fang@gmail.com>
2025-11-06 00:27:43 -0800

    2.4 fps game clock

--- main.py
+++ main.py
@@ -3,12 +3,11 @@ from constants import *
 from logger import log_state
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     # start pygame
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     # game loop
     while True:
@@ -23,6 +22,8 @@ def main():
         # Render
         screen.fill((0, 0, 0))
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
