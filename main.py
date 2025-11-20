22f5f74b43a3891914436cca9cb6c298d0e31fa0
jfang <im.jonathan.fang@gmail.com>
2025-11-20 11:12:03 -0800

    3.5

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
 
     # game loop
@@ -22,10 +26,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        updatable.update(dt)
         # Render
         screen.fill((0, 0, 0))
-        player.draw(screen)
+        for draw in drawable:
+            draw.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
