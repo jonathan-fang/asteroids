db8993349c2405e8f08d37ee73a1ab988d3a7886
jfang <im.jonathan.fang@gmail.com>
2025-11-25 11:59:20 -0800

    deactivate zerg rush bullets 4.4

--- player.py
+++ player.py
@@ -8,6 +8,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.cooldown_timer = 0
     
     # in the Player class
     def triangle(self):
@@ -31,6 +32,10 @@ class Player(CircleShape):
         self.position += rotated_with_speed_vector
     
     def shoot(self):
+        if self.cooldown_timer > 0:
+            return
+            # prevent player from shooting
+        self.cooldown_timer = PLAYER_SHOOT_COOLDOWN_SECONDS
         # at position of player, create new Shot
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = pygame.Vector2(0, 1)
@@ -39,6 +44,7 @@ class Player(CircleShape):
         shot.velocity += rotated_with_speed_vector
     
     def update(self, dt):
+        self.cooldown_timer -= dt
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
